c05d5d60bb352725760ec3dfaedd0e227409d194
Zach Marshall <marshallz2018@gmail.com>
2022-03-10 12:39:46 -0700

    Change from sqlite to mysql

--- app.py
+++ app.py
@@ -1,30 +1,37 @@
 import datetime
-import sqlite3
+import flask_mysqldb
 
 from flask import Flask, render_template, request
 
 app = Flask(__name__, static_url_path='/static/')
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'cs_495'
+
+mysql = flask_mysqldb.MySQL(app)
+
 checkList = []
 
 def setUpCheckList():
     global checkList
-    connection = sqlite3.connect("SPAMM.db")
-    cursor = connection.cursor()
+    cursor = mysql.connection.cursor()
 
-    count = cursor.execute("select count(*) from sex")      # number of options that the sex could be
-    sexCount = count.fetchone()
-    checkList.append(sexCount[0])
+    cursor.execute("select count(*) from sex")      # number of options that the sex could be
+    count = cursor.fetchone()[0]
+    checkList.append(count)
 
-    count = cursor.execute("select count(*) from race")     # number of options that the race could be
-    raceCount = count.fetchone()
-    checkList.append(raceCount[0])
+    cursor.execute("select count(*) from race")     # number of options that the race could be
+    count = cursor.fetchone()[0]
+    checkList.append(count)
 
     checkList.append(100)                                   # I don't think we'll have anyone older than 100
 
-    count = cursor.execute("select count(*) from majors")   # number of options that the major could be
-    majorCount = count.fetchone()
-    checkList.append(majorCount[0])
-    checkList.append(majorCount[0])                         # a second time for the second major
+    cursor.execute("select count(*) from majors")   # number of options that the major could be
+    count = cursor.fetchone()[0]
+    checkList.append(count)
+    checkList.append(count)                                 # a second time for the second major
 
     checkList.append(99)                                    # grad year should not be more than double digits
 
@@ -53,14 +60,13 @@ def checkString(s):
             if not (0 < intData[i] <= checkList[i]):    # Taking advantage of python if statements
                 raise Exception
 
-        connection = sqlite3.connect("SPAMM.db")        # testing 'code' for getting an event
+        cursor = mysql.connection.cursor()              # testing 'code' for getting an event
                                                         # (see if 'code' is for real event)
-        cursor = connection.cursor()
-        count = cursor.execute("select id from events where code = " + str(code) + ' and "'
+
+        cursor.execute("select id from events where code = " + str(code) + ' and "'
                                + str(datetime.datetime.now().replace(microsecond=0))
                                + '" between startTime and endTime')
-        event = count.fetchone()
-        if not event:
+        if not cursor.fetchone():
             raise Exception
 
         return True
@@ -69,8 +75,7 @@ def checkString(s):
         return False
 
 def putInDatabase(s):
-    connection = sqlite3.connect("SPAMM.db")
-    cursor = connection.cursor()
+    cursor = mysql.connection.cursor()
 
     data = s.split('/')                         # formatting the sql query
     code = data.pop()
@@ -79,17 +84,17 @@ def putInDatabase(s):
     for i in range(1,len(data)):
         values += ', ' + data[i]
                                                 # put event foreign key id in place of the code in the string
-    event = cursor.execute("select id from events where code = " + str(code) + ' and "'
-                           + str(datetime.datetime.now().replace(microsecond=0))
-                           + '" between startTime and endTime')
-    eventID = event.fetchone()[0]
+    cursor.execute("select id from events where code = " + str(code) + ' and "'
+                       + str(datetime.datetime.now().replace(microsecond=0))
+                       + '" between startTime and endTime')
+    eventID = cursor.fetchone()[0]
 
     values += ', ' + str(eventID)
     sql = "INSERT INTO attendance (stuID, sex, race, age, major, major2, gradYear, event)" \
           " VALUES (" + values + ");"
 
     cursor.execute(sql)
-    connection.commit()
+    mysql.connection.commit()
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
